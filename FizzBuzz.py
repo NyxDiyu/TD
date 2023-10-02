0e753b5acac26b90cb81fe2b02edabf725ffbf3d
Nyx.Diyu <nyx.diyu@skiff.com>
2023-10-02 15:27:36 +0200

    Fizzbuzz resultat

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,3 +1,15 @@
 class FizzBuzz:
-    def affiche(self):
-        pass
+    def affiche(self, nombre):
+        resultat=""
+
+        for i in range(1, nombre + 1):
+            if i%15==0 :
+                resultat += "Fizz"
+            elif i%5==0 :
+                resultat += "Lol"
+            elif i%3==0 :
+                resultat += "IsLol"
+            else:
+                resultat += str(i)
+                
+        return resultat
