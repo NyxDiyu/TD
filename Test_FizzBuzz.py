c3e6cb6aeb2c087f37f25a9ca16a0a81792c1ab0
Nyx.Diyu <nyx.diyu@skiff.com>
2023-10-02 15:22:29 +0200

    Fizzbuzz modif nombre

--- Test_FizzBuzz.py
+++ Test_FizzBuzz.py
@@ -1,13 +1,18 @@
 import unittest
 from FizzBuzz import *
 
-class Test_FizzBuzz(unittest.TestCase)
+class Test_FizzBuzz(unittest.TestCase):
 
     def setUp(self):
         self.instance=FizzBuzz()
 
     def test_affiche_sans_param(self):
-        self.assertEqual(self.instance.affiche(), "fizzlol")
+        with self.subTest(self):
+            self.assertEqual(self.instance.affiche(100), "fizzIsLol")
+        with self.subTest(self):
+            self.assertEqual(self.instance.affiche(5), "fizzlol")
+        with self.subTest(self):
+            self.assertEqual(self.instance.affiche(3), "fizz")
 
 if __name__ == '__main__':
     unittest.main()
