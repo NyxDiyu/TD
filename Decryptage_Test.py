f8d0c2f99eb3d1247e3991e34bc1e470e6bc6c2c
Nyx.Diyu <nyx.diyu@skiff.com>
2023-10-02 16:23:39 +0200

    Decryptage Td deuxieme évolution

---
+++ Decryptage_Test.py
@@ -0,0 +1,12 @@
+import unittest
+from Decryptage import *
+
+class Cryptage_Test(unittest.TestCase):
+    def setUp(self):
+        self.instance=Decryptage()
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
